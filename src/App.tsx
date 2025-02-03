9f21e78a0b69dd399e7d0c70a9fe7b283c919460
rqphy <raphael.ferreira@hetic.net>
2025-02-03 22:51:06 +0100

    :sparkles: update default scene with shadow and a floor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
 import Experience from "./components/Experience"
 
 function App() {
-	return (
-		<>
-			<Canvas shadows camera={{ position: [8, 8, 8], fov: 30 }}>
-				<color attach="background" args={["#ececec"]} />
-				<OrbitControls />
-
-				<Experience />
-			</Canvas>
-		</>
-	)
+    return (
+        <>
+            <Experience />
+        </>
+    )
 }
 
 export default App
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,30 @@
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+
 export default function Experience() {
-	return (
-		<mesh>
-			<boxGeometry args={[2, 2, 2]} />
-			<meshBasicMaterial color={"red"} />
-		</mesh>
-	)
+    return (
+        <Canvas shadows camera={{ position: [8, 8, 8], fov: 30 }}>
+            <OrbitControls />
+            <color attach="background" args={["#ececec"]} />
+            <ambientLight />
+            <directionalLight
+                castShadow
+                position={[2, 2, -2]}
+                intensity={4.5}
+            />
+
+            <mesh castShadow position={[0, 0.5, 0]}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color={"red"} />
+            </mesh>
+            <mesh
+                receiveShadow
+                rotation={[-0.5 * Math.PI, 0, 0]}
+                position={[0, 0, 0]}
+            >
+                <circleGeometry args={[3, 32]} />
+                <meshStandardMaterial color={"pink"} />
+            </mesh>
+        </Canvas>
+    )
 }
